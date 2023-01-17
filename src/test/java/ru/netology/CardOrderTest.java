1782cf4002dcaa068f39470fafc0cd4d320ed172
Anfisa Golovanova <anfisa.faa@gmail.com>
2023-01-17 19:04:56 +0700

    initial commit 2

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome,driver", "./driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void SetUp() { driver= new ChromeDriver(); }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTest1() throws  InterruptedException {
+        driver.get("http://localhost:9999");
+
+    }
+}
